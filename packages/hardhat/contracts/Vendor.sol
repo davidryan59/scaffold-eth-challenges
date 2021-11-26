82a417cd2df3fae654bd67050c4e01b52dbd74bd
David Ryan <dr-github@outlook.com>
2021-11-26 14:39:33 +0700

    Checkpoint 3 complete Vendor

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,22 +1,31 @@
 pragma solidity 0.8.4;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
+contract Vendor is Ownable {
 
   YourToken yourToken;
   uint256 public constant tokensPerEth = 100;
+
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   constructor(address tokenAddress) public {
     yourToken = YourToken(tokenAddress);
   }
 
-
   // ToDo: create a payable buyTokens() function:
-  
+  function buyTokens() public payable {
+    uint tokensToBuy = msg.value * tokensPerEth;
+    require(tokensToBuy <= yourToken.balanceOf(address(this)), "Vendor does not have enough tokens");
+    yourToken.transfer(msg.sender, tokensToBuy);
+    emit BuyTokens(msg.sender, msg.value, tokensToBuy);
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public onlyOwner {
+    payable(msg.sender).transfer(address(this).balance);
+  }
   
 
   // ToDo: create a sellTokens() function:
