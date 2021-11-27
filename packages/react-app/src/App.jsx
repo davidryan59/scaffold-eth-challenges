97aea946301ca903fedf17b93cd3f307582df895
David Ryan <dr-github@outlook.com>
2021-11-27 11:49:41 +0700

    Tweak info

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -765,7 +765,7 @@ function App(props) {
       </div>
 
       <div style={{ marginTop: 32, opacity: 0.5 }}>
-        Created by <Address value={"Your...address"} ensProvider={mainnetProvider} fontSize={16} />
+        Created by <Address value={"dryan.eth"} ensProvider={mainnetProvider} fontSize={16} />
       </div>
 
       <div style={{ marginTop: 32, paddingBottom: 128, opacity: 0.5 }}>
