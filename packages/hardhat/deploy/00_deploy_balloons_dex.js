bad204b9c3a64de36446c2de964360e83a37e640
David Ryan <dr-github@outlook.com>
2021-12-03 08:25:55 +0700

    Checkpoint 2 deployment script

--- packages/hardhat/deploy/00_deploy_balloons_dex.js
+++ packages/hardhat/deploy/00_deploy_balloons_dex.js
@@ -18,16 +18,19 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log: true,
   });
 
-  // const dex = await ethers.getContract("DEX", deployer);
+  const dex = await ethers.getContract("DEX", deployer);
+
+  console.log("Deployer address:");
+  console.log(deployer);
 
   // paste in your address here to get 10 balloons on deploy:
-  // await balloons.transfer("YOUR_ADDRESS",""+(10*10**18));
+  await balloons.transfer("0x808febA97F0BA70B7f8EF1e6e4DD199C07D011Ec",""+(10*10**18));
 
-  // uncomment to init DEX on deploy:
-  // console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
-  // If you are going to the testnet make sure your deployer account has enough ETH
-  // await balloons.approve(dex.address,ethers.utils.parseEther('100'));
-  // console.log("INIT exchange...")
-  // await dex.init(""+(3*10**18),{value:ethers.utils.parseEther('3'),gasLimit:200000})
+  // // uncomment to init DEX on deploy:
+  console.log("Approving DEX ("+dex.address+") to take Balloons from main account...");
+  // // If you are going to the testnet make sure your deployer account has enough ETH
+  await balloons.approve(dex.address,ethers.utils.parseEther('100'));
+  console.log("INIT exchange...");
+  await dex.init(""+(3*10**18),{value:ethers.utils.parseEther('3'),gasLimit:200000});
 };
 module.exports.tags = ["Balloons", "DEX"];
