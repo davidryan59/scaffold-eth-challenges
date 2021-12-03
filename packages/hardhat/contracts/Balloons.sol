8b85555c2dd589fcf0ce079fb0a93972f8d7ab61
David Ryan <dr-github@outlook.com>
2021-12-03 08:25:31 +0700

    Minting self some balloons

--- packages/hardhat/contracts/Balloons.sol
+++ packages/hardhat/contracts/Balloons.sol
@@ -7,7 +7,7 @@ contract Balloons is ERC20 {
   constructor() ERC20("Balloons", "BAL") {
       // **You can update the msg.sender address with your 
       // front-end address to mint yourself tokens.
-      _mint(0x697D940BC9A2aa8F56c1eC65E640951781C98A23, 1000 ether);
+      _mint(0x808febA97F0BA70B7f8EF1e6e4DD199C07D011Ec, 1000 ether);
       // This mints to the deployer
       _mint(msg.sender, 1000 ether);
   }
