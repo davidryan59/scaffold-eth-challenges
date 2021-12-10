588a75198ad6efc2c53f852a057de20245aec733
David Ryan <dr-github@outlook.com>
2021-12-10 14:55:15 +0700

    Default to dark mode

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -99,7 +99,7 @@ const walletLinkProvider = walletLink.makeWeb3Provider(`https://mainnet.infura.i
 const web3Modal = new Web3Modal({
   network: "mainnet", // Optional. If using WalletConnect on xDai, change network to "xdai" and add RPC info below for xDai chain.
   cacheProvider: true, // optional
-  theme: "light", // optional. Change to "dark" for a dark theme.
+  theme: "dark", // optional. Value "light" or "dark"
   providerOptions: {
     walletconnect: {
       package: WalletConnectProvider, // required
